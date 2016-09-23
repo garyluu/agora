a42138d401c33925b5298de644857c3dab6e7ab4
Jacqueline Roberti <jroberti@wmbaf-e93.broadinstitute.org>
2016-09-23 14:44:55 -0400

    fix git sha

--- build/build-images.sh
+++ build/build-images.sh
@@ -2,7 +2,9 @@
 
 set -eux
 
-GIT_SHA=$(git rev-parse ${CIRCLE_BRANCH}:$PROJECT)
+GIT_SHA=$(git rev-parse ${CIRCLE_BRANCH})
+
+echo "LABEL org.broadinstitute.$PROJECT.git-sha=$GIT_SHA" >> Dockerfile
 
 # Create new docker image
 docker build -t $REPO:${GIT_SHA:0:12} $PROJECT
