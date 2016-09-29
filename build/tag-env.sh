ac505aaa95b2fb6e39d626de0ba0f749af5dc945
Jackie Roberti <jacqueline.roberti93@gmail.com>
2016-09-29 14:15:00 -0400

    move GIT_SHA out of env vars

--- build/tag-env.sh
+++ build/tag-env.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#GIT_SHA=$(git rev-parse ${BRANCH})
-echo $GIT_SHA
-echo $REPO
+GIT_SHA=$(git rev-parse ${BRANCH})
 
 docker pull $REPO:${GIT_SHA:0:12}
 docker tag $REPO:${GIT_SHA:0:12} $REPO:$ENV
 docker push $REPO:$ENV
+
+# update hash in submodules
+ruby updatehash.rb
